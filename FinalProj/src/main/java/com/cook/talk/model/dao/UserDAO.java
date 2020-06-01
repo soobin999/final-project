1bd26d53707bb169ae51c64813b0bc3bf7f90fc3
yujin_309 <jyujin1102@gmail.com>
2020-06-01 16:17:19 +0900

    ㅎㅎ

--- FinalProj/src/main/java/com/cook/talk/model/dao/UserDAO.java
+++ FinalProj/src/main/java/com/cook/talk/model/dao/UserDAO.java
@@ -1,5 +1,12 @@
 package com.cook.talk.model.dao;
 
-public interface UserDAO {
+import com.cook.talk.model.VO.UserVO;
 
+public interface UserDAO {
+	/*public void insertUser(UserVO user);
+public int userIdCheck(String userId);
+public int userNickCheck(String nickName);
+public int updateUser(UserVO user);
+*/
+void join(UserVO userVO) throws Exception;
 }
