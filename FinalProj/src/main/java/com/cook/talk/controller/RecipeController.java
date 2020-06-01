6da90495ed90fcaeff9ed97eed902f3d5bc18ca9
Harry9509 <kumho9509@gmail.com>
2020-06-01 16:02:26 +0900

    server.port=80 spring.datasource.driver-class-name=com.mysql.cj.jdbc.Driver spring.datasource.url=jdbc:mysql://localhost:3306/recipe?serverTimezone=UTC spring.datasource.username=root spring.datasource.password=1234

--- FinalProj/src/main/java/com/cook/talk/controller/RecipeController.java
+++ FinalProj/src/main/java/com/cook/talk/controller/RecipeController.java
@@ -1,15 +1,17 @@
 package com.cook.talk.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class RecipeController {
-	
+
 	@RequestMapping("/newlist")
-	public String getRecipeList(Model model);
-		model.addattribute();
-	
-	
+	public String getRecipeList(Model model) {
+
+		model.addAttribute("", "");
+		return "";
+	}
 
 }
--- FinalProj/src/main/java/com/cook/talk/model/dao/AdUserDAO.java
+++ FinalProj/src/main/java/com/cook/talk/model/dao/AdUserDAO.java
@@ -2,13 +2,14 @@ package com.cook.talk.model.dao;
 
 import java.util.List;
 
+import com.cook.talk.model.VO.UserVO;
 import com.cook.talk.model.dto.UserDTO;
 
 public interface AdUserDAO {
 	public List<UserDTO> allSelectUserId();
-	public void deleteUserID(UserDTO userId);
-	public void updateUserNickName(UserDTO userNick);
-	public void searchUserByEmail(UserDTO userEmail);
+	public void deleteUserID(UserVO userId);
+	public void updateUserNickName(UserVO nickName);
+	public void searchUserByEmail(UserVO userEmail);
 
 }
 	
