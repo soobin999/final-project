907ab0263ba1450026992a1e806fa6d9a73aff91
Harry9509 <kumho9509@gmail.com>
2020-06-03 10:26:49 +0900

    나

--- FinalProj/src/test/java/com/cook/talk/ck/HYujin.java
+++ FinalProj/src/test/java/com/cook/talk/ck/HYujin.java
@@ -13,24 +13,26 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class HYujin {
 
-	@Autowired
-	UserDAO userDao;
-	
-	@Test
-	public void find() {
-		log.info(userDao.findUserById("abc@naver.com").toString());
-	}
-	
-	@Test
-	public void save() {
-		UserVO user = new UserVO(null, null, null, null, 0, false, false, null, 0);
-		user.setUserId("abcd@naver.com");
-		user.setUserPw("1234");
-		user.setNickName("유찌");
-		user.getBirth();
-		user.setGender(true);
-		user.setAccess(true);
-		user.setAuth(0);
-	}
+   @Autowired
+   UserDAO userDao;
+   
+   @Test
+   public void find() {
+      log.info(userDao.findUserById("abc@naver.com").toString());
+   }
+   
+   @Test
+   public void save() {
+      UserVO user = new UserVO();
+      user.setUserId("abcd@naver.com");
+      user.setUserPw("1234");
+      user.setNickName("유찌");
+      user.getBirth();
+      user.setGender(true);
+      user.setAccess(true);
+      user.setAuth(0);
+      
+      userDao.save(user);
+      
+   }
 }
-
